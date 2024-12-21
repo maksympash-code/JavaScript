918ae5c5f34cd800ec66f50950a5ab1ecd1b14af
Pashchenko Maksym <maksympash@gmail.com>
2024-12-22 00:05:41 +0200

    lesson_4(Mathematical operations and methods)

--- www/js/main.js
+++ www/js/main.js
@@ -13,15 +13,40 @@
 // var number;
 // number = 5;
 
-const number = 5;
-// number = 10;
-console.log("Змінна: " + number + ".");
+// const number = 5;
+// // number = 10;
+// console.log("Змінна: " + number + ".");
 
-var num;
-num = true;
-num = "Речення";
-console.log("Значення: " + num);
+// var num;
+// num = true;
+// num = "Речення";
+// console.log("Значення: " + num);
 
-var num_1 = 5;
-var num_2 = "5";
-console.log(num_1 + num_2);
+// var num_1 = 5;
+// var num_2 = "5";
+// console.log(num_1 + num_2);
+
+
+// lesson_4
+var num_1 = 5.4;
+var num_2 = 15;
+
+res = num_1 - num_2;
+res = num_1 + num_2;
+res = num_1 * num_2;
+res = num_1 / num_2;
+console.log("Результат: " + res)
+
+var num_3 = 5;
+num_3 += 7;
+num_3--;
+console.log("Результат: " + num_3)
+
+var str_1 = Number("12"); // class Number
+var str_2 = Number("2");
+console.log("Result: " + (str_1 + str_2));
+
+console.log("Math: " + Math.PI);
+console.log("Math: " + Math.E);
+console.log("Minimum: " + Math.min(4, 6, -8, 2, 0, 5, 8))
+console.log("Max: " + Math.max(4, 6, -8, 2, 67, 5, 8))
