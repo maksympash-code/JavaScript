8f04a857cb8c46132273da53e7c8d0e6225942a1
Pashchenko Maksym <maksympash@gmail.com>
2024-12-21 23:32:42 +0200

    lesson_2(Output to the console)

---
+++ www/js/main.js
@@ -0,0 +1,5 @@
+// document.write("JavaScript говорить мені") // застарілий метод
+// console.log("JavaScript говорить мені");
+// console.info("JavaScript говорить мені")
+// console.error("JavaScript говорить мені")
+// console.warn("JavaScript говорить мені")
