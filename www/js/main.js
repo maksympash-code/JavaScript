d33c383a25c258fde83511dfcaa095ace89100b3
Pashchenko Maksym <maksympash@gmail.com>
2024-12-21 23:48:58 +0200

    lesson_3(variables)

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,27 @@
-// document.write("JavaScript говорить мені") // застарілий метод
+// lesson_2
+
+// document.write("JavaScript говорить мені"); // застарілий метод
 // console.log("JavaScript говорить мені");
-// console.info("JavaScript говорить мені")
-// console.error("JavaScript говорить мені")
-// console.warn("JavaScript говорить мені")
+// console.info("JavaScript говорить мені");
+// console.error("JavaScript говорить мені");
+// console.warn("JavaScript говорить мені");
+
+
+
+// lesson_3
+
+// var number;
+// number = 5;
+
+const number = 5;
+// number = 10;
+console.log("Змінна: " + number + ".");
+
+var num;
+num = true;
+num = "Речення";
+console.log("Значення: " + num);
+
+var num_1 = 5;
+var num_2 = "5";
+console.log(num_1 + num_2);
